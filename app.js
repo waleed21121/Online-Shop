cde793dee0b7633a416d55f4515a8f1bff260c2c
waleed <wa581509@gmail.com>
2024-08-07 13:07:10 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({path : 'config.env'});
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'assets')));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.get('/', (req, res, next) => {
+    res.send('hello');
+})
+
+const PORT = process.env.PORT;
+app.listen(PORT, (err) => {
+    console.log(`server listening on port ${PORT}`);
+})
