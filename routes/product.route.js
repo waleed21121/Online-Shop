2b26ab7d6a1f8a939dd8d8369a95677d093c8e65
waleed <wa581509@gmail.com>
2024-08-09 00:14:29 +0300

    added route to get the product by its id

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
 
+const prouctController = require('../controllers/product.controller');
+
+router.get('/product/:productId', prouctController.getProduct);
 
 modules.export = router;
