ae59f71b7b709dd315cb571923501927f4a713fa
waleed <wa581509@gmail.com>
2024-08-08 19:55:30 +0300

    edited getHome function to filter by category

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -3,6 +3,15 @@ const productsModel = require('../models/products.model');
 exports.getHome = async(req, res, next) => {
     // Get products
     // Render index.ejs file
-    let products = await productsModel.getAllProducts();
-    res.render('index', {products: products});
+
+    //filter by category
+    let category = req.query.category;
+    if(category && category !== 'all') {
+        let products = await productsModel.getProductsByCategories(category);
+        res.render('index', {products: products});
+    }
+    else {
+        let products = await productsModel.getAllProducts();
+        res.render('index', {products: products});
+    }
 }
