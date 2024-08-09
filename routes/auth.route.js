1ccf42cc779ed5d50eee8e587351af1dd0da8b49
waleed <wa581509@gmail.com>
2024-08-09 12:17:30 +0300

    login route added

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const authController = require('../controllers/auth.controller');
 
 router.get('/signup', authController.getSignup);
-router.pst('/signup', bodyParser.urlencoded({extended: true}), authController.postSignup);
+router.post('/signup', bodyParser.urlencoded({extended: true}), authController.postSignup);
+router.get('/login', authController.getlLogin);
 
 module.exports = router;
