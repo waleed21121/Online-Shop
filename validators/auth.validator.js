6615e5ddc9b496dd094cffd9eda15bc0f954758e
waleed <wa581509@gmail.com>
2024-08-10 23:25:17 +0300

    added login validator handler

--- validators/auth.validator.js
+++ validators/auth.validator.js
@@ -1,16 +1,22 @@
 const check = require('express-validator').check;
 
-exports.createAccountValidator = [
-    check('username').notEmpty().withMessage('Name is required')
-        .isAlpha('en-US', {ignore: ' '}).withMessage('Name must contain only alphabetic characters'),
-    check('email').isEmail().withMessage('Invalid Email format')
-        .isEmail({host_whitelist : ['gmail.com']}).withMessage('Email must end with @gmail.com'),
-    check('password')
-        .isLength({ min: 8 }).withMessage('Password must contain at least 8 characters'),
-    check('confirmPassword').custom((value, { req }) => {
-        if (value !== req.body.password) {
-        return false;
-        }
-        return true;
-    }).withMessage('Password confirmation does not match password')
-];
+const checkUsername = () => check('username').notEmpty().withMessage('Name is required')
+                                .isAlpha('en-US', {ignore: ' '}).withMessage('Name must contain only alphabetic characters');
+
+const checkEmail = () => check('email').isEmail().withMessage('Invalid Email format')
+                            .isEmail({host_whitelist : ['gmail.com']}).withMessage('Email must end with @gmail.com');
+
+const checkPassword = () => check('password')
+                                .isLength({ min: 8 }).withMessage('Password must contain at least 8 characters');
+
+const checkConfirmPassword = () => check('confirmPassword').custom((value, { req }) => {
+                                        if (value !== req.body.password) {
+                                            return false;
+                                        }
+                                        return true;
+                                    }).withMessage('Password confirmation does not match password');
+
+
+exports.createAccountValidator = [checkUsername(), checkEmail(), checkPassword(), checkConfirmPassword()];
+
+exports.loginValidator = [checkEmail(), checkPassword()];
