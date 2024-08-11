837d52abaedc13f4e26bae823c9e96d0fd70f91a
waleed <wa581509@gmail.com>
2024-08-11 13:30:45 +0300

    created file cart router

---
+++ routes/cart.route.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const bodyParser = require('body-parser');
+const cartController = require('../controllers/cart.controller');
+
+module.exports = router;
