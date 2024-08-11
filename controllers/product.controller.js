b7c1f94bbd6e87ff372470b2755e242131fce367
waleed <wa581509@gmail.com>
2024-08-11 15:12:00 +0300

    added hidden inputs represents the product properties at product.ejs file

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,10 +3,10 @@ const productModel = require('../models/products.model');
 exports.getProduct = async (req, res, next) => {
     let id = req.params.id;
     let product = await productModel.getProductById(id);
-    res.render('product', {product: product});
+    res.render('product', {product: product, isUser: req.session.userId});
 }
 
 exports.getFirstproduct = async (req, res, next) => {
     let product = await productModel.getFirstProduct();
-    res.render('product', {product : product});
+    res.render('product', {product : product, isUser: req.session.userId});
 }
