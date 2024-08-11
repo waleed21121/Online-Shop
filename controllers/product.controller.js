1fde1af06816dff16ffa0bd053918b57ef66ded2
waleed <wa581509@gmail.com>
2024-08-11 15:27:44 +0300

    display of validation errors of product amount at product.ejs

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,7 +3,7 @@ const productModel = require('../models/products.model');
 exports.getProduct = async (req, res, next) => {
     let id = req.params.id;
     let product = await productModel.getProductById(id);
-    res.render('product', {product: product, isUser: req.session.userId});
+    res.render('product', {product: product, isUser: req.session.userId, validationErrors: req.flash('validationErrors')[0]});
 }
 
 exports.getFirstproduct = async (req, res, next) => {
