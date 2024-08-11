05bded9c7363d7893db8e423631860b8c4b51880
waleed <wa581509@gmail.com>
2024-08-11 14:10:50 +0300

    added post cart router with validation and router protection

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 const bodyParser = require('body-parser');
 const cartController = require('../controllers/cart.controller');
+const authGard = require('../guards/auth.guard');
+const cartValidator = require('../validators/cart.validator');
+
+router.use(bodyParser.urlencoded({extended: true}));
+
+router.post('/', authGard.isAuth, cartValidator.checkAmount, cartController.postCart);
 
 module.exports = router;
