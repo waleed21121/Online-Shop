c484b65240536e523cb74e022e2e6f475e2f4bbd
waleed <wa581509@gmail.com>
2024-08-12 16:22:41 +0300

    added get all orders request handler

--- routes/orders.route.js
+++ routes/orders.route.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 const bodyParser = require("body-parser");
 const orderController = require('../controllers/order.controller');
-const authGard = require('../guards/auth.guard');
+const authGuard = require('../guards/auth.guard');
 
 router.use(bodyParser.urlencoded({extended: true}));
 
-router.get('/verify-order', authGard.isAuth, orderController.getOrderVerify);
+router.get('/verify-order', authGuard.isAuth, orderController.getOrderVerify);
 
-router.post('/', authGard.isAuth, orderController.postOrder);
+router.get('/',  authGuard.isAuth, orderController.getOrder);
+
+router.post('/', authGuard.isAuth, orderController.postOrder);
 
 module.exports = router;
