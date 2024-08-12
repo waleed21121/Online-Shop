9ed1479d1e8c5f928113c7f831a688f8966803ec
waleed <wa581509@gmail.com>
2024-08-12 17:01:07 +0300

    check the address validation errors

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,5 +1,6 @@
 const orderModel = require('../models/oreder.model');
 const cartModel = require('../models/cart.model');
+const validationResult = require('express-validator').validationResult;
 
 exports.getOrderVerify = async (req, res, next) => {
     const item = await cartModel.getItemById(req.query.order);
@@ -11,17 +12,27 @@ exports.getOrderVerify = async (req, res, next) => {
 };
 
 exports.postOrder = async (req, res, next) => {
-    await orderModel.addNewOrder({
-        name: req.body.name,
-        price: req.body.price,
-        amount: req.body.amount,
-        userId: req.body.userId,
-        productId: req.body.productId,
-        timestamp: Date.now(),
-        address: req.body.address,
-        status: "pending"
-    }, req.body.cartId);
-    res.redirect("/orders");
+    const validationErrors = validationResult(req);
+    if(validationErrors.isEmpty()) {
+        await orderModel.addNewOrder({
+            name: req.body.name,
+            price: req.body.price,
+            amount: req.body.amount,
+            userId: req.body.userId,
+            productId: req.body.productId,
+            timestamp: Date.now(),
+            address: req.body.address,
+            status: "pending"
+        }, req.body.cartId);
+        res.redirect("/orders");
+    } else {
+        const errorsArray = validationErrors.array().map(err => ({
+            param: err.path,
+            msg: err.msg
+        }));
+        req.flash('validationErrors', errorsArray);
+        res.redirect("/orders/verify-order?order=" + req.body.cartId);
+    }
 };
 
 exports.getOrder = async (req, res, next) => {
