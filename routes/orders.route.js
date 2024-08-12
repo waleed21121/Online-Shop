fec95d5fbd09cea1929430732bb80012238ff33f
waleed <wa581509@gmail.com>
2024-08-12 16:55:17 +0300

    added address validator to the router

--- routes/orders.route.js
+++ routes/orders.route.js
@@ -2,12 +2,13 @@ const router = require("express").Router();
 const bodyParser = require("body-parser");
 const orderController = require('../controllers/order.controller');
 const authGuard = require('../guards/auth.guard');
+const addressValidation = require('../validators/address.validator');
 
 router.use(bodyParser.urlencoded({extended: true}));
 
 router.get('/verify-order', authGuard.isAuth, orderController.getOrderVerify);
 
-router.get('/',  authGuard.isAuth, orderController.getOrder);
+router.get('/', addressValidation.addressValidator, authGuard.isAuth, orderController.getOrder);
 
 router.post('/', authGuard.isAuth, orderController.postOrder);
 
