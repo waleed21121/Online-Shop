61eafffce78cec95245f1a9edf1ce3743d5cd412
waleed <wa581509@gmail.com>
2024-08-13 12:22:14 +0300

    added multer to get the input files(images)

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,12 +1,18 @@
 const router = require("express").Router();
 const adminController = require('../controllers/admin.controller');
-const bodyParser = require('body-parser');
-const adminGuard = require('../guards/admin.guard');
-
-router.use(bodyParser.urlencoded({extended: true}));
+const multer = require('multer');
 
 router.get('/add', adminGuard.isUserAdmin, adminController.getAdd);
 
-router.post('/add', adminGuard.isUserAdmin, adminController.postAdd);
+router.post('/add', adminGuard.isUserAdmin, multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, 'images');
+        },
+        filename: function (req, file, cb) {
+            cb(null, Date.now() + '-' + file.originalname);
+        }
+    })
+}).single('image'), adminController.postAdd);
 
 module.exports = router
