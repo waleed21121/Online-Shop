1331b3266f0f8092404acbe0128faed64e0a528b
waleed <wa581509@gmail.com>
2024-08-13 12:56:50 +0300

    added get add product page middleware

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,3 +1,6 @@
+const productsModel = require('../models/products.model');
+const validationResult = require('express-validator').validationResult;
+
 exports.getAdd = (req, res, next) => {
     res.render('add-product', {
         validationErrors: req.flash('validationErrors'),
@@ -5,3 +8,19 @@ exports.getAdd = (req, res, next) => {
         isUser: true
     });
 };
+
+exports.getPost = async (req, res, next) => {
+    const validationErrors = validationResult(req);
+    if(validationErrors.isEmpty()) {
+        req.body.image = req.file.filename;
+        await productsModel.addNewProduct(req.body)
+        res.redirect("/admin/add");
+    } else {
+        const errorsArray = validationErrors.array().map(err => ({
+            param: err.path,
+            msg: err.msg
+        }));
+        req.flash("validationErrors", errorsArray);
+        res.redirect("/admin/add");
+    }
+}
